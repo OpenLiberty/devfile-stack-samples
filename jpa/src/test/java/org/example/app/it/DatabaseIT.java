d32cafb60b5f10402eaa3ca81047706914403913
Edward Mezarina <mezarina@us.ibm.com>
2021-03-06 20:52:08 -0500

    Code review
    Signed-off-by: Edward Mezarina <mezarina@us.ibm.com>

--- jpa/src/test/java/org/example/app/it/DatabaseIT.java
+++ jpa/src/test/java/org/example/app/it/DatabaseIT.java
@@ -21,136 +21,60 @@ package org.example.app.it;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.InputStream;
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.Collection;
+import java.util.Collections;
 
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.example.app.Person;
-import org.junit.jupiter.api.AfterAll;
+import org.example.app.PersonResource;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DatabaseIT {
-
-	private static Jsonb jsonb;
-	private static Client client;
+	private static Logger log = LoggerFactory.getLogger(DatabaseIT.class);
+	private static PersonResource personSvc;
 
 	@BeforeAll
-	public static void init() {
-		client = ClientBuilder.newClient();
-		jsonb = JsonbBuilder.newBuilder().build();
-	}
-
-	@AfterAll
-	public static void destroy() throws Exception {
-		jsonb.close();
-		client.close();
-	}
-
-	public WebTarget getTarget(String path) {
-		String port = System.getProperty("http.port");
-		String context = System.getProperty("context.root");
-		String url = "http://localhost:" + port + "/" + context + "/";
-		return client.target(url + path);
+	public static void init() throws Exception {
+		URI uri = UriBuilder.fromUri("http://localhost:"+System.getProperty("http.port")).path(System.getProperty("app.path")).build();
+		log.info("URL: " + uri);
+		personSvc = JAXRSClientFactory.create(uri.toString(), PersonResource.class, Collections.singletonList(JacksonJsonProvider.class));
 	}
 
 	@Test
 	public void testGetPerson() {
-		// Add a person to the database.
 		Person bob = new Person("Bob", 24);
-		WebTarget target = getTarget("").queryParam("name", bob.getName()).queryParam("age", bob.getAge());
-		Response postResponse = target.request().post(null);
-		Long personId;
-		try {
-			assertEquals(200, postResponse.getStatus(), "Request received an invalid status response. Details: " + postResponse.getStatusInfo().getReasonPhrase());
-			personId = jsonb.fromJson(postResponse.readEntity(InputStream.class), Long.class);
-			bob.setId(personId);
-		} finally {
-			postResponse.close();
-		}
+		Long bobId = personSvc.createPerson("Bob", 24);
+		bob.setId(bobId);
 
-		// Retrieve the database entry.
-		target = getTarget(personId.toString());
-		Response getResponse = target.request().get();
-		try {
-			assertEquals(200, getResponse.getStatus(), "Request received an invalid status response. Details: " + getResponse.getStatusInfo().getReasonPhrase());
-			Person person = jsonb.fromJson(getResponse.readEntity(InputStream.class), Person.class);
-			assertEquals(bob, person);
-		} finally {
-			getResponse.close();
-		}
+		Person person = personSvc.getPerson(bobId);
+		assertEquals(bob, person);
 
-		// Remove the datase entry.
-		target = getTarget(personId.toString());
-		Response delResponse = target.request().delete();
-		try {
-			assertEquals(200, delResponse.getStatus(), "Request received an invalid status response. Details: " + delResponse.getStatusInfo().getReasonPhrase());
-		} finally {
-			delResponse.close();
-		}
+		personSvc.removePerson(bobId);
 	}
 
 	@Test
 	public void testGetAllPeople() {
-		// Add two people to the database.
 		Person mary = new Person("Mary", 1);
-		WebTarget target = getTarget("").queryParam("name", mary.getName()).queryParam("age", mary.getAge());
-		Response post1Response = target.request().post(null);
-
-		try {
-			assertEquals(200, post1Response.getStatus(), "Request received an invalid status response. Details: " + post1Response.getStatusInfo().getReasonPhrase());
-			Long maryId = jsonb.fromJson(post1Response.readEntity(InputStream.class), Long.class);
-			mary.setId(maryId);
-		} finally {
-			post1Response.close();
-		}
-
+		Long maryId = personSvc.createPerson(mary.getName(), mary.getAge());
+		mary.setId(maryId);
 		Person james = new Person("James", 2);
-		target = getTarget("").queryParam("name", james.getName()).queryParam("age", james.getAge());
-		Response post2Response = target.request().post(null);
-		try {
-			assertEquals(200, post2Response.getStatus(), "Request received an invalid status response. Details: " + post2Response.getStatusInfo().getReasonPhrase());
-			Long jamesId = jsonb.fromJson(post2Response.readEntity(InputStream.class), Long.class);
-			james.setId(jamesId);
-		} finally {
-			post2Response.close();
-		}
+		Long jamesId = personSvc.createPerson(james.getName(), james.getAge());
+		james.setId(jamesId);
 
-		// Retrieve the database entries.
-		Response getResponse = target.request().get();
-		try {
-			assertEquals(200, getResponse.getStatus(), "Request received an invalid status response. Details: " + getResponse.getStatusInfo().getReasonPhrase());
-			Collection<Person> people = jsonb.fromJson(getResponse.readEntity(InputStream.class),new ArrayList<Person>() {}.getClass().getGenericSuperclass());
+		Collection<Person> people = personSvc.getAllPeople();
+		assertTrue(people.size() >= 2, "Expected at least 2 people to be registered, but there were only: " + people.size());
+		assertTrue(people.contains(mary));
+		assertTrue(people.contains(james));
 
-			assertTrue(people.size() >= 2, "Expected at least 2 people to be registered, but there were only: " + people.size());
-			assertTrue(people.contains(mary));
-			assertTrue(people.contains(james));
-		} finally {
-			getResponse.close();
-		}
-
-		// Remove the datase entries.
-		target = getTarget(String.valueOf(mary.getId()));
-		Response del1Response = target.request().delete();
-		try {
-			assertEquals(200, del1Response.getStatus(), "Request received an invalid status response. Details: " + del1Response.getStatusInfo().getReasonPhrase());
-		} finally {
-			del1Response.close();
-		}
-
-		target = getTarget(String.valueOf(james.getId()));
-		Response del2response = target.request().delete();
-		try {
-			assertEquals(200, del2response.getStatus(), "Request received an invalid status response. Details: " + del2response.getStatusInfo().getReasonPhrase());
-		} finally {
-			del2response.close();
-		}
+		personSvc.removePerson(maryId);
+		personSvc.removePerson(jamesId);
 	}
 }
